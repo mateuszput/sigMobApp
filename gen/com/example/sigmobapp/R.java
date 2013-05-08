9708dab00a9cd451b62d5d65127ec9f9d6926c7b
Mateusz Put <mateusz.put@gmail.com>
2013-05-08 20:58:25 +0200

    buttons

--- gen/com/example/sigmobapp/R.java
+++ gen/com/example/sigmobapp/R.java
@@ -15,14 +15,17 @@ public final class R {
     }
     public static final class id {
         public static final int appVersionTextView=0x7f070004;
-        public static final int btnClose=0x7f070008;
+        public static final int btnClose=0x7f07000b;
+        public static final int btnCyclicTasks=0x7f070009;
+        public static final int btnMesssage=0x7f070008;
         public static final int btnNextScreen=0x7f070002;
-        public static final int menu_settings=0x7f070009;
+        public static final int btnPreferences=0x7f07000a;
+        public static final int btnSurveysList=0x7f070007;
+        public static final int menu_settings=0x7f07000c;
         public static final int messagesTextView=0x7f070003;
         public static final int my_root=0x7f070005;
         public static final int name=0x7f070000;
         public static final int password=0x7f070001;
-        public static final int txtEmail=0x7f070007;
         public static final int txtName=0x7f070006;
     }
     public static final class layout {
--- src/pl/edu/agh/sigmobapp/MenuActivity.java
+++ src/pl/edu/agh/sigmobapp/MenuActivity.java
@@ -18,18 +18,18 @@ public class MenuActivity extends Activity {
         setContentView(R.layout.activity_menu);
  
         TextView txtName = (TextView) findViewById(R.id.txtName);
-        TextView txtEmail = (TextView) findViewById(R.id.txtEmail);
+//        TextView txtEmail = (TextView) findViewById(R.id.txtEmail);
         Button btnClose = (Button) findViewById(R.id.btnClose);
  
         Intent i = getIntent();
         // Receiving the Data
         String name = i.getStringExtra("name");
         String apikey = i.getStringExtra("apikey");
-        Log.e("Second Screen", name + "." + apikey);
+//        Log.e("Second Screen", name + "." + apikey);
  
         // Displaying Received data
-        txtName.setText(name);
-        txtEmail.setText(apikey);
+        txtName.setText("Your login: " + name);
+//        txtEmail.setText(apikey);
  
         // Binding Click event to Button
         btnClose.setOnClickListener(new View.OnClickListener() {
