45a90839644862970ed821cca9808e134578976e
Krzysztof Kot <krzykot@gmail.com>
2013-05-12 09:26:26 +0200

    wylaczenie strictmode - problemy z polaczeniem na android 4.0

--- src/pl/edu/agh/sigmobapp/MainActivity.java
+++ src/pl/edu/agh/sigmobapp/MainActivity.java
@@ -19,6 +19,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -43,6 +44,11 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		StrictMode.ThreadPolicy policy = new StrictMode.
+		ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy); 
+		
 		setContentView(R.layout.activity_login);
 		sigmobProperties = SigmobProperties.getInstance();
 		
--- src/pl/edu/agh/sigmobapp/SListActivity.java
+++ src/pl/edu/agh/sigmobapp/SListActivity.java
@@ -18,6 +18,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -30,6 +31,11 @@ public class SListActivity extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+		StrictMode.ThreadPolicy policy = new StrictMode.
+		ThreadPolicy.Builder().permitAll().build();
+		StrictMode.setThreadPolicy(policy); 
+        
         setContentView(R.layout.activity_slist);
  
         sigmobProperties = SigmobProperties.getInstance();
